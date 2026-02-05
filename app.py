7339bd626ed0f8c5f31acbf809c821dc07cd3294
TheNootLinja <thenootlinja@gmail.com>
2026-02-05 12:38:33 -0500

    Add code for generating date time

--- app.py
+++ app.py
@@ -5,13 +5,16 @@
 # Options: New, View Old
 
 # New allows user to write out an entry
+
+import datetime
+
 print('Write a journal entry:' + "\n")
 new_journal_title = input("What do you want to title this journal?")
 new_journal_content = input()
 
 f = open('journal.txt', "a")
 f.write(
-    new_journal_title + " - 02/05/2026 @ 12:26" + "\n" +
+    new_journal_title + " - " + str(datetime.datetime.now().strftime("%m/%d/%Y %H:%M:%S")) + "\n" +
     new_journal_content + "\n\n"
 )
 f.close()
